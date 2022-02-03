502e26569a9ff20787202bb29012b20c50fbec78
unknown <Igor Calvo>
2022-02-03 00:56:48 -0200

    addind entry for every missing day and todo list

--- main.py
+++ main.py
@@ -10,14 +10,36 @@ data = pd.read_csv(csvFileName)
 data = CreateEntry(data)
 data.to_csv(csvFileName, index=False)
 
-# layout = [[sg.Text("Hello from PySimpleGUI")], [sg.Button("OK")]]
-# window = sg.Window("Demo", layout)
-#
-# while True:
-#     event, values = window.read()
-#     # End program if user closes window or
-#     # presses the OK button
-#     if event == "OK" or event == sg.WIN_CLOSED:
-#         break
-#
-# window.close()
+layout = [[sg.Text("Hello from PySimpleGUI")], [sg.Button("OK")]]
+window = sg.Window("Demo", layout)
+
+while True:
+    event, values = window.read()
+    if event == "OK" or event == sg.WIN_CLOSED:
+        break
+
+window.close()
+
+# TO LIST
+# -----------
+# LAYOUT
+#   size pixels
+#   checkbox
+#   control checkbox
+#   exit button
+#   phrase
+#   pop up message on exit
+#   style
+#   icon
+
+# LOGIC
+#   save checkboxes to csv
+#   read variables from another csv
+#   create new data.csv file if changed columns (preserve old data if possible)
+
+# MISC
+#   add sounds?
+#   find phrases to display
+
+# DOWN THE ROAD
+#   graphs clickable UI data insights
--- utils.py
+++ utils.py
@@ -1,12 +1,19 @@
-from datetime import date
+from datetime import date, timedelta
 
-def CheckForTodaysEntry(lastDate: str) -> bool:
+def CheckForTodaysEntry(lastDate: str) -> int:
     delta = date.today() - date.fromisoformat(lastDate)
-    return delta.days >= 1
+    return delta.days
+
+def GetLatestDate(data) -> str:
+    return data.iloc[-1]['date']
 
 def CreateEntry(data):
-    if CheckForTodaysEntry(data.iloc[-1]['date']):
-        di = dict.fromkeys(data.columns.values, False)
-        di['date'] = str(date.today())
-        data = data.append((di), ignore_index=True)
+    lastDate: str = GetLatestDate(data)
+    deltaDays = CheckForTodaysEntry(data.iloc[-1]['date'])
+    if deltaDays > 1:
+        di = dict.fromkeys(data.columns.values, 'False')
+        for day in range(deltaDays):
+            newDate = date.fromisoformat(lastDate)
+            di['date'] = str(newDate + timedelta(days=(day + 1)))
+            data = data.append((di), ignore_index=True)
     return data
